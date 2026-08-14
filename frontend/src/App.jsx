817264afc441a6966a99939c6350f20b0c34c5f1
rommel dalisay <rommelmdalisay@gmail.com>
2026-08-14 08:00:26 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+
+        <div>
+
+          <p>hello world</p>
+        </div>
+
+      </section>
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
